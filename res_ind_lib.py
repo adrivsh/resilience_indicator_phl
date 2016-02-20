8e557c254fa4e674099ab09a4ef291ddfe403c6c
adrivsh <adrien.vogt.schilb@gmail.com>
2016-02-19 19:54:16 -0500

    improves demo

--- res_ind_lib.py
+++ res_ind_lib.py
@@ -263,3 +263,47 @@ def def_ref_values(df):
     df["v_s"] = vr
     df["pov_head_ref"]=df["pov_head"]
     return df
+    
+#function
+def make_tiers(series,labels=["Low","Mid","High"]):
+    return pd.cut(series,[series.min()-1e3]+series.quantile([1/3,2/3]).tolist()+[series.max()+1e3],labels=labels).sort_values() #This magically orders the in the "Low", "Mid", "High" order, i wonder why
+
+def categories_to_formated_excel_file(df_cat,filename="tiers.xlsx"):
+    #Outputs resilience tiers to excel
+    with pd.ExcelWriter(filename, engine='xlsxwriter') as writer:
+        workbook=writer.book
+        # Add a format. Light red fill with dark red text.
+        red = workbook.add_format({#'bg_color': '#f4a582',
+                                       'font_color': '#ca0020'})
+        orange = workbook.add_format({#'bg_color': '#f7f7f7',
+                                       'font_color': '#000000'})
+
+        blue = workbook.add_format({#'bg_color': '#92c5de',
+                                       'font_color': '#0571b0'})
+        df_cat.to_excel(writer,sheet_name="tiers")
+        worksheet=writer.sheets["tiers"]
+        worksheet.conditional_format('B2:B600', {'type':     'text',
+                                       'criteria': 'containing',
+                                    'value':    "Low",
+                                    'format':   red})
+        writer.sheets["tiers"].conditional_format('B2:B600', {'type':     'text',
+                                    'criteria': 'containing',
+                                    'value':    "High",
+                                    'format':   blue})
+        writer.sheets["tiers"].conditional_format('C2:C600', {'type':     'text',
+                                       'criteria': 'containing',
+                                    'value':    "Low",
+                                    'format':   blue})
+        writer.sheets["tiers"].conditional_format('C2:C600', {'type':     'text',
+                                    'criteria': 'containing',
+                                    'value':    "High",
+                                    'format':   red})
+        writer.sheets["tiers"].conditional_format('B2:C600', {'type':     'text',
+                                    'criteria': 'containing',
+                                    'value':    "Mid",
+                                    'format':   orange}) 
+        worksheet.autofilter('A1:C1')
+        worksheet.set_column(0, 0, 20)
+        worksheet.set_column(1, 1, 14)
+        worksheet.set_column(2, 2, 8)
+        writer.sheets["tiers"].freeze_panes(1, 1)
